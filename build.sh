4d18917240ddef0e2a5348cb3901687ee420c53a
Justin Fagnani <justinfagnani@google.com>
2015-05-11 10:20:38 -0700

    Use dom5 for parsing.

--- build.sh
+++ build.sh
@@ -2,5 +2,4 @@
 
 browserify -e node_modules/css -s css -o vendor/css.js
 browserify -e node_modules/dom5 -s dom5 -o vendor/dom5.js
-browserify -e node_modules/parse5 -s parse5 -o vendor/parse5.js
-#browserify -x vendor/dom5.js vendor/parse5.js -e node_modules/hydrolysis -s hydrolysis -o vendor/hydrolysis.js
+browserify -x vendor/dom5.js -e node_modules/hydrolysis -s hydrolysis -o vendor/hydrolysis.js
